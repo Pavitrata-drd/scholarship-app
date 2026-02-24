59c1c121a800f851691f67baf3622149e2ff2cee
muthubrinda1305 <muthubrinda47@gmail.com>
2026-02-24 23:21:02 +0530

    Resolve stash conflict and apply local changes

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.https://mcranuklhopiynlvnzqk.supabase.co
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jcmFudWtsaG9waXlubHZuenFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNTI3MTEsImV4cCI6MjA4NjgyODcxMX0.7xsHW0pvslnoeIHpnOFGU8nzGvYwN68bcRMx-KLLMJk
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
